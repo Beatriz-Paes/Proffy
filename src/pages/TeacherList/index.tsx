71b80ac84f928dd636101c547e22fc0209e8e54d
Beah <beah.dev@gmail.com>
2020-10-09 18:27:02 -0400

    Componete PageHeader e estilizaçao da TeacherList

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+import './style.css';
 
 function TeacherList () {
   return (
-      <h1>Teachers</h1>
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os Proffys disponíveis">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week-day">Dia da semana</label>
+            <input type="text" id="subject" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" id="subject" />
+          </div>
+        </form>
+      </PageHeader>
+    </div>
   );
 }
 
