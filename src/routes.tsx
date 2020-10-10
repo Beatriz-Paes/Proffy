bcd86a82e09990fe0873dec91f8428f924e0a385
Weslley <weslleynfs@gmail.com>
2020-10-10 01:12:35 -0400

    Surrounding das Routes com Switch e substituiçao provisoria do Link do react-router-dom pela anchor do HTML (tag <a>)

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Landing from './pages/Landing';
-import TeacherForm from './pages/TeacherForm';
-import TeacherList from './pages/TeacherList';
+import React from "react";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import Landing from "./pages/Landing";
+import TeacherForm from "./pages/TeacherForm";
+import TeacherList from "./pages/TeacherList";
 
 function Routes() {
-  return(
+  return (
     <BrowserRouter>
-    <Route path="/" exact component={Landing} />
-    <Route path="/study" component={TeacherList} />
-    <Route path="/give-classes" component={TeacherForm} />
-    </BrowserRouter>  
+      <Switch>
+        <Route path="/" exact component={Landing}></Route>
+        <Route path="/study" component={TeacherList}></Route>
+        <Route path="/give-classes" component={TeacherForm}></Route>
+        <Redirect path="*" to="/"></Redirect>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
